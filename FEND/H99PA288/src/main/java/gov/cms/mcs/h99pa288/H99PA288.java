0f455bbc37863575a1903135c06863b24f7e1549
Noah Hefner <noah.hefner127@gmail.com>
2023-04-21 20:55:54 -0400

    initial commit

---
+++ FEND/H99PA288/src/main/java/gov/cms/mcs/h99pa288/H99PA288.java
@@ -0,0 +1,13 @@
+package gov.cms.mcs.h99pa288;
+
+public class H99PA288 {
+
+    public H99PA288 () {}
+
+    public void exec () {
+
+        System.out.println("Hello from H99PA288!");
+    
+    } 
+
+}
---
+++ FEND/H99PA288/src/main/java/gov/cms/mcs/h99pa288/H99PA288Driver.java
@@ -0,0 +1,12 @@
+package gov.cms.mcs.h99pa288;
+
+public class H99PA288Driver {
+
+    public static void main (String[] args) {
+
+        H99PA288 proc = new H99PA288();
+        proc.exec();
+
+    }
+
+}
