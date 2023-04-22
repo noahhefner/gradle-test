25cc6398e40e0b65360715ba966f482eed224727
Noah Hefner <noah.hefner127@gmail.com>
2023-04-21 22:33:28 -0400

    inter sub-project dependencies

---
+++ Common/Constants/src/main/java/gov/cms/mcs/constants/Constants.java
@@ -0,0 +1,9 @@
+package gov.cms.mcs.constants;
+
+public final class Constants {
+
+    private Constants () {}
+
+    public static final String PRINT_ME = "Constants string";
+
+}
--- FEND/H99PA288/src/main/java/gov/cms/mcs/h99pa288/H99PA288.java
+++ FEND/H99PA288/src/main/java/gov/cms/mcs/h99pa288/H99PA288.java
@@ -1,5 +1,7 @@
 package gov.cms.mcs.h99pa288;
 
+import gov.cms.mcs.constants.Constants;
+
 public class H99PA288 {
 
     public H99PA288 () {}
@@ -7,7 +9,8 @@ public class H99PA288 {
     public void exec () {
 
         System.out.println("Hello from H99PA288!");
+        System.out.println(Constants.PRINT_ME);
     
-    } 
+    }
 
 }
