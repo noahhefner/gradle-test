358941afea266c44daa4892c01f163e0216c5258
Noah Hefner <noah.hefner127@gmail.com>
2023-04-24 08:24:58 -0400

    other modules

---
+++ FEND/Z99PA288/src/main/java/gov/cms/mcs/z99pa288/Z99PA288.java
@@ -0,0 +1,13 @@
+package gov.cms.mcs.z99pa288;
+
+public class Z99PA288 {
+
+    public Z99PA288 () {}
+
+    public void exec () {
+
+        System.out.println("Hello from Z99PA288!");
+    
+    }
+
+}
---
+++ FEND/Z99PA288/src/main/java/gov/cms/mcs/z99pa288/Z99PA288Driver.java
@@ -0,0 +1,12 @@
+package gov.cms.mcs.z99pa288;
+
+public class Z99PA288Driver {
+
+    public static void main (String[] args) {
+
+        Z99PA288 proc = new Z99PA288();
+        proc.exec();
+
+    }
+
+}
